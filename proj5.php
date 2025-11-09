220a5e7c7f312dbe66f2a6a2e905b32ae273a7a8
tvv3 <tvv363@gmail.com>
2025-11-09 18:02:08 +0200

    proj 5 agenda c# photos added and minor changes made in proj5.php

--- proj5.php
+++ proj5.php
@@ -1,21 +1,19 @@
 <?php
 $i=5;
-$titlu="Agenda";//se fol in title si in #abouProject
+$titlu="Agenda C#";//se fol in title si in #abouProject
 $demolink="#";
 $descriere_about="Realizat cu C# si baza de date SQLITE.";//se fol in sectiunea descriere adica #aboutProject
 $descriere_detaliata_proiect="Proiectul Agenda este realizat in C# si baza de date SQLITE. In ecranul principal se salveaza site-urile cu posibilitatea alegerii dintr-o tabela de categorii (form separat). Se vizualizeaza site-urile introduse intr-un grid cu paginare si exista posibilitatea de a cauta / filtra site-urile.";
 //$imgfull="assets/img/portfolio/proj".$i."/fullsize/"; //cale proj
 //$imgthmb="assets/img/portfolio/proj".$i."/thumbnails/";//cale proj
-$nr=1;//number of images
-$imgdescr[1]=$titlu." - 1";
-/*$imgdescr[2]="Sala Gym 8 - 2";
-$imgdescr[3]="Sala Gym 8 - 3";
-
-$imgdescr[4]="Sala Gym 8 - 4";
-$imgdescr[5]="Sala Gym 8 - 5";
-$imgdescr[6]="Sala Gym 8 - 6";*/
-
-$alt=array(1=>"alt1");
+$nr=12;//number of images
+$imgdescr=[];
+$alt=[];
+for($j=1; $j<=$nr;$j++)
+{
+$imgdescr[$j]=$titlu." - ".$j;
+$alt[$j]="alt".$j;
+}
 
 require("views/proj-master.php");
 ?>
