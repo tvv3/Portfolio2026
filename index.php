81ba05e339fda41d366fac005c7c4de607b05ca1
tvv3 <tvv363@gmail.com>
2025-11-10 18:37:28 +0200

    mainNav static in styles2

--- index.php
+++ index.php
@@ -25,17 +25,17 @@
     </head>
     <body id="page-top">
         <!-- Navigation-->
-        <nav class="navbar navbar-expand-lg py-3" id="mainNav">
+        <nav class="navbar navbar-expand-lg navbar-light py-3" id="mainNav">
             <div class="container px-4 px-lg-5">
                 <a class="navbar-brand" href="index.php#acasa-top">Portofoliul</a>
                 <button class="navbar-toggler navbar-toggler-right" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
                 <div class="collapse navbar-collapse" id="navbarResponsive">
                     <ul class="navbar-nav ms-auto my-2 my-lg-0">
-                        <li class="nav-item"><a class="nav-link" href="#acasa-top">Acasa</a></li>
-                        <li class="nav-item"><a class="nav-link" href="#about">Despre noi</a></li>
-                        <li class="nav-item"><a class="nav-link" href="#services1">Tehnologii</a></li>
-                        <li class="nav-item"><a class="nav-link" href="#portfolio-section1">Portofoliu</a></li>
-                        <li class="nav-item"><a class="nav-link" href="#contact1">Contact</a></li>
+                        <li class="nav-item"><a class="nav-link" href="index.php#acasa-top">Acasa</a></li>
+                        <li class="nav-item"><a class="nav-link" href="index.php#about">Despre noi</a></li>
+                        <li class="nav-item"><a class="nav-link" href="index.php#services1">Tehnologii</a></li>
+                        <li class="nav-item"><a class="nav-link" href="index.php#portfolio-section1">Portofoliu</a></li>
+                        <li class="nav-item"><a class="nav-link" href="index.php#contact1">Contact</a></li>
                     </ul>
                 </div>
             </div>
--- views/proj-master.php
+++ views/proj-master.php
@@ -50,7 +50,7 @@ if ((!isset($demolink))) $demolink="#";
     </head>
     <body id="page-top">
         <!-- Navigation-->
-        <nav class="navbar navbar-expand-lg py-3" id="mainNav">
+        <nav class="navbar navbar-expand-lg navbar-light py-3" id="mainNav">
             <div class="container px-4 px-lg-5">
                 <a class="navbar-brand" href="index.php#acasa-top">Portofoliul</a>
                 <button class="navbar-toggler navbar-toggler-right" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
